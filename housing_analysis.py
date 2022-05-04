f513beb890c24947db79ac0943eca762f3869124
lauralardies <lrodrlar@myuax.com>
2022-05-04 16:37:51 +0200

    código finalizado

--- housing_analysis.py
+++ housing_analysis.py
@@ -2,28 +2,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Para ver qué variables vamos a estudiar, calcularemos su correlación lineal y la representaremos en una gráfica.
-# A parir de ahí representaremos las variables que consideraremos oportunas a partir del estudio anterior.
+# ---------------------------------
+# ¡ANÁLISIS COMPLETO EN EL READ ME!
+# ---------------------------------
 
 def correlacion(datos, n, saveas):
     correlation_matrix = datos.corr().round(n)
     sns.heatmap(data=correlation_matrix, annot=True)
     plt.savefig(("img/" + saveas + ".png"), bbox_inches = "tight")
 
-def diagrama (var1, var2, saveas):
+def diagrama (var1, var2, correlacion, saveas):
 
     plt.figure()
     sns.lmplot(x = var1, y = var2, data = df)
-    plt.savefig(("img/" + saveas + ".png"))
+    plt.savefig(("img/" + correlacion + "/" + saveas + ".png"))
 
-df = pd.read_csv("USA_Housing.csv")
+df = pd.read_csv("USA_Housing.csv") # Creamos nuestro Dataframe con la ayuda de pandas.
 
-correlacion(df, 1, "Antes") # Calculamos y graficamos la correlación lineal
+correlacion(df, 1, "Antes") # Calculamos y graficamos la correlación lineal.
 
-# Descartamos las variables que tienen poca influencia, correlacion <= 0.4
+# Descartamos las variables que tienen poca influencia, correlacion <= 0,4.
 df.pop("Avg. Area Number of Rooms")
 df.pop("Avg. Area Number of Bedrooms")
 
-correlacion(df, 2, "Después") # Calculamos y graficamos la nueva correlación lineal después que a ver eliminado las variables.
+correlacion(df, 2, "Después") # Calculamos y graficamos la nueva correlación lineal después de haber eliminado las variables.
 
-# Por lo tanto, sabemos que las gráficas que van a tener correlación son las siguientes: 
+# CORRELACIÓN. 
+
+# 1 - Ingresos - Precio.
+diagrama("Avg. Area Income", "Price", "correlación", "Ingresos - Precio")
+
+# 2 - Edad Casa - Precio.
+diagrama("Avg. Area House Age", "Price", "correlación", "Edad Casa - Precio")
+
+# 3 - Población - Precio.
+diagrama("Area Population", "Price", "correlación", "Población- Precio")
+
+# NO CORRELACIÓN. 
+
+# 1 - Edad Casa - Ingresos.
+diagrama("Avg. Area House Age", "Avg. Area Income", "no correlación", "Edad Casa - Ingresos")
+
+# 2 - Edad Casa - Población.
+diagrama("Avg. Area House Age", "Area Population", "no correlación", "Edad Casa - Población")
+
+# 3 - Ingresos - Población.
+diagrama("Avg. Area Income", "Area Population", "no correlación", "Ingresos - Población")
